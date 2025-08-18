35a7852ba315019f0472a96ed81b04c0d2118a4a
LucasSilvadO <lucasoliveira999@cs.unifran.edu.br>
2025-08-18 20:29:21 -0300

    atualização do script

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ function calcularSimples() {
     // vazio .. '' , null , undefined
 
     if (document.getElementById("capital").value == ''){ // Se condição
-        naoPreenchida = 'Capital'; // faça isso
+        naoPreenchida = '"Capital"'; // faça isso
     }
     else if (document.getElementById("taxa").value == '') { // Senão Se
-        naoPreenchida = 'Taxa'; // faça isso
+        naoPreenchida = '"Taxa"'; // faça isso
     }
     else if (document.getElementById("tempo").value == ''){
-        naoPreenchida = 'Tempo';
+        naoPreenchida = '"Tempo"';
     }
 
     // if (naoPreenchida != ''){
@@ -44,10 +44,44 @@ function calcularComposto() {
     let capital = parseFloat(document.getElementById("capital").value);
     let taxa = parseFloat(document.getElementById("taxa").value) / 100;
     let tempo = parseFloat(document.getElementById("tempo").value);
+    let naoPreenchida = ''
 
-    let montante = capital * Math.pow((1 + taxa), tempo);
+    // Este código verifica se os dados foram preenchidos
+    // vazio .. '' , null , undefined
+
+    if (document.getElementById("capital").value == ''){ // Se condição
+        naoPreenchida = '"Capital"'; // faça isso
+    }
+    else if (document.getElementById("taxa").value == '') { // Senão Se
+        naoPreenchida = '"Taxa"'; // faça isso
+    }
+    else if (document.getElementById("tempo").value == ''){
+        naoPreenchida = '"Tempo"';
+    }
+
+    // if (naoPreenchida != ''){
+    //     alert('Não preencheu ' + naoPreenchida);
+    // }
+
+    // Estrutura condicionel
+    // & = and
+    // || = or
+    
+    if (naoPreenchida != ''){
+        document.getElementById("resultado_falha").innerHTML = 
+        `Dados não preenchidos ${naoPreenchida}`;  
+        
+        document.getElementById("resultado").innerHTML = "";
+    }
+    else{
+        let montante = capital * Math.pow((1 + taxa), tempo);
+        document.getElementById("resultado").innerHTML = 
+        `Montante com Juros Compostos: R$ ${montante.toFixed(2)}`;
+
+        document.getElementById("resultado_falha").innerHTML = "";
+    } 
 
     document.getElementById("resultado").innerHTML = 
         `Montante com Juros Compostos: R$ ${montante.toFixed(2)}`;
-   
+
 }
